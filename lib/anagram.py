c1639afc97e3239174ef6c63e21a3ce8c2a98072
Isaac <bennisaac24@gmail.com>
2025-02-24 22:42:08 +0300

    final commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, anagram):
+        self.anagram = anagram.lower()
+
+    def match(self, words):
+        main_sorted = sorted(self.anagram)
+
+        matching_words = [word for word in words if sorted(word.lower()) == main_sorted]
+
+        return matching_words
+
+
+word1 = Anagram("enlist")
+print(word1.match(["listen", "poison", "hello"]))
